e9ad9c17d8a4627d23e8d48e8faa53b2152cbbe0
Dragonfire <bastian616-git@yahoo.de>
2017-01-25 23:48:17 +0100

    Simple Stomp Test Client

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,49 @@ import ReactDOM from 'react-dom';
 export default class TestClient extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            channel: '',
+            message: '',
+        }
+        this.initClient();
+        this.send = this.send.bind(this);
+        this.handleChannel = this.handleChannel.bind(this);
+        this.handleMessage = this.handleMessage.bind(this);
     }
 
-    componentDidMount() {
-        var socket = new SockJS('echo.websocket.org');
-        stompClient = Stomp.over(socket);
-        stompClient.connect({}, function (frame) {
-            setConnected(true);
-            console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/greetings', function (greeting) {
-                console.log(JSON.parse(greeting.body).content);
-            });
-        });
+    initClient() {
+        this.client = Stomp.client("ws://localhost:8080/gs-guide-websocket", "v11.stomp");
+        this.client.connect("", "",
+            function () {
+                this.client.subscribe("/topic/greetings",
+                    function (message) {
+                        console.log(message);
+                    },
+                    {});
+            }
+        );
+    }
+
+    send() {
+        this.client.send(this.state.channel, this.state.message);
+    }
+
+
+    handleChannel(event) {
+        this.setState({ channel: event.target.value });
+    }
+
+    handleMessage(event) {
+        this.setState({ message: event.target.value });
     }
 
     render() {
         return <div>
-            Test Da
+            <label>Channel</label>
+            <input value={this.state.channel} onChange={this.handleChannel} />
+            <label>Message</label>
+            <input value={this.state.message} onChange={this.handleMessage} />
+            <button onClick={this.send}>Send Message</button>
         </div>;
     }
 }
